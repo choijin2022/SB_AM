75c12be3eb883af4622de2fc4ec6d0367c18effd
choijin2022 <choeji2022@gmail.com>
2022-12-06 19:46:47 +0900

    spring boot _ article, member

---
+++ src/main/java/com/cji/exam/demo/vo/Article.java
@@ -0,0 +1,17 @@
+package com.cji.exam.demo.vo;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Article {
+	private int id;
+	private LocalDateTime regDate;
+	private String title;
+	private String body;
+}
---
+++ src/main/java/com/cji/exam/demo/vo/Member.java
@@ -0,0 +1,33 @@
+package com.cji.exam.demo.vo;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+//	private int id;
+//	private String loginId;
+//	private String loginPw;
+//	private String name;
+//	private String nickname;
+//	private String cellphoneNum;
+//	private String email;
+	private int id ;
+	private LocalDateTime regDate;
+	private LocalDateTime updateDate; 
+	private int loginId;
+	private String loginPw; 
+	private int authLevel; 
+	private String name;
+	private String nickname;
+	private String cellphoneNum; 
+	private String email; 
+	private int delStatus; 
+	private String delDate; 
+	
+}
